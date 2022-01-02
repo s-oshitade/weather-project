e567ac1f2e6f06d65bed9a0c0be98ad77417bb06
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-02 14:11:49 +0000

    Hide API key!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const https = require("https");
 const bodyParser = require("body-parser")
+require('dotenv').config();
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res)=> {
   const query = req.body.cityName;
-  const apiKey = "672d74be94a1b0f30d72b0c19b62e215"
+  const apiKey = process.env.API_KEY;
   const unit = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
   https.get(url, (response) => {
